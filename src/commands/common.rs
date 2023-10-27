f24bdf64947bb4885697ac386ba4175b2920ecd7
Vaurkhorov <43547976+Vaurkhorov@users.noreply.github.com>
2023-10-27 12:21:57 +0530

    fixed existing repo check
    fixed typo while checking if the repository already exists

--- src/commands/common.rs
+++ src/commands/common.rs
@@ -1,8 +1,9 @@
 use crate::Error;
 use std::fs;
 
+/// Checks if a repository already exists in the current working directory
 pub fn kifi_exists() -> Result<(), Error> {
-    match fs::read_dir("./kifi") {
+    match fs::read_dir("./.kifi") {
         Ok(_) => Ok(()),
         Err(_) => Err(Error::KifiNotInitialised),
     }
